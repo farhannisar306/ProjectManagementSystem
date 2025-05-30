79797eef0465f77ae2bcdbd96d843ffcfe4507c0
Modulus Development <80498735+farhannisar306@users.noreply.github.com>
2025-05-30 18:12:58 +0600

    PMS-004: User Creation Validation added

--- backend/src/app/controllers/user/user.route.ts
+++ backend/src/app/controllers/user/user.route.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { createUser, deleteUser, updateUser } from './user.controller';
 import { validate } from '../../middlewares/validate';
-import { userSchema } from './user.validate';
+import { userCreationSchema, userUpdateSchema } from './user.validate';
 
 const userRouter = Router();
 
-userRouter.post('/create', createUser);
+userRouter.post('/create', validate(userCreationSchema), createUser);
 userRouter.delete('/delete/:id', deleteUser);
-userRouter.patch('/update/:id', validate(userSchema), updateUser);
+userRouter.patch('/update/:id', validate(userUpdateSchema), updateUser);
 
 export default userRouter;
--- backend/src/app/controllers/user/user.validate.ts
+++ backend/src/app/controllers/user/user.validate.ts
@@ -1,5 +1,16 @@
 import { z } from "zod";
-export const userSchema = z.object({
+
+export const userCreationSchema = z.object({
+    email: z.string().email(),
+    firstname: z.string().min(3),
+    lastname: z.string().min(3),
+    password: z.string().min(6),
+    avatar: z.string().url().optional(),
+    bio: z.string().min(10),
+    phone: z.string().min(10),
+})
+
+export const userUpdateSchema = z.object({
     email: z.string().email(),
     firstname: z.string().min(3),
     lastname: z.string().min(3),
